6c66a3c08903a255499641ea1c55316cc29bac86
EshaanS3 <156532619+EshaanS3@users.noreply.github.com>
2025-09-13 23:28:26 -0500

    Update resource.tsx

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,6 +3,26 @@ import ResourceCard from '../../components/ResourceCard';
 const resources = [
   { title: 'Donate Life Registry', url: 'https://donatelife.net/', description: 'Register as a donor or manage your consent.' },
   { title: 'OrganDonor.gov', url: 'https://www.organdonor.gov/', description: 'Official federal information on organ donation.' },
+  { 
+  title: 'SODA Executive Board Application', 
+  url: 'https://docs.google.com/forms/d/e/1FAIpQLSdy6lvfRXlHa0a_eqta2WorTPJTMu9r0HndYatquVy-dBlEMw/closedform', 
+  description: 'Apply to join the SODA Stevenson Executive Board.' 
+},
+{ 
+  title: 'Register as a Donor – SODA at Stevenson High School', 
+  url: 'https://registerme.org/campaign/sodastevensonhs', 
+  description: 'Sign up through the Donate Life registry and support our Stevenson HS campaign.' 
+},
+{ 
+  title: 'Stevenson SODA Volunteer Form', 
+  url: 'https://docs.google.com/forms/d/e/1FAIpQLSeVQmzXfT0uUZwgXNWqKf8IOcrwi7EwTOogdID4eom92k7TIA/viewform', 
+  description: 'Get involved with SODA Stevenson as a volunteer.' 
+},
+{ 
+  title: 'Stevenson SODA Instagram', 
+  url: 'https://www.instagram.com/stevensonsoda/?hl=en', 
+  description: 'Follow us on Instagram for updates, events, and stories.' 
+},
 ];
 
 export default function ResourcesPage() {
